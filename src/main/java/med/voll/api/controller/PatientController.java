127a02b0dd6d72f4d17b9c4c9b3536229f88b817
Danton Ponce <dantonponce@MacBook-Air-de-Danton.local>
2024-10-12 17:49:31 -0300

    Developing tha function do list the patients with pagination

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,13 +1,16 @@
 package med.voll.api.controller;
 
 import med.voll.api.patients.DataCreatePatients;
+import med.voll.api.patients.DataPatientList;
 import med.voll.api.patients.Patient;
 import med.voll.api.patients.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -20,6 +23,11 @@ public class PatientController {
     public void create(@RequestBody DataCreatePatients data){
         // System.out.println(data);
         repository.save(new Patient(data));
+    }
+
+    @GetMapping
+    public Page<DataPatientList> list(@PageableDefault(size=10, sort={"name"}) Pageable pagination){
+        return repository.findAll(pagination).map(DataPatientList::new);
     }
 
 }
---
+++ src/main/java/med/voll/api/patients/DataPatientList.java
@@ -0,0 +1,14 @@
+package med.voll.api.patients;
+
+public record DataPatientList(
+        Long id,
+        String name,
+        String email,
+        String cpf
+) {
+
+    public DataPatientList(Patient patient){
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getCpf());
+    }
+
+}
